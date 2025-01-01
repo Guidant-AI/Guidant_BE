69fcc507f87617f7108a1af73d59e7a08fb22e3d
Prashant Mishra <40368212+programsmagic@users.noreply.github.com>
2025-01-01 23:57:44 +0530

    pm2 start up script updated

--- start_up_script.sh
+++ start_up_script.sh
@@ -23,14 +23,16 @@ pip install -r requirements.txt
 
 # Start the FastAPI application with PM2
 echo "Starting the FastAPI application with PM2..."
-pm2 start uvicorn --name "fastapi-app" -- main:app --host 0.0.0.0 --port 8000
+# pm2 start uvicorn --name "fastapi-app" -- main:app --host 0.0.0.0 --port 8000
+pm2 start "uvicorn main:app --host 0.0.0.0 --port 8000" --name "fastapi-app" --interpreter python3
+
 
 # Save the PM2 process list
 echo "Saving PM2 process list..."
 pm2 save
 
-# Set PM2 to start on system reboot
-echo "Configuring PM2 to start on system reboot..."
-pm2 startup --silent
+# # Set PM2 to start on system reboot
+# echo "Configuring PM2 to start on system reboot..."
+# pm2 startup --silent
 
-echo "FastAPI app deployed and managed with PM2!"
+# echo "FastAPI app deployed and managed with PM2!"
